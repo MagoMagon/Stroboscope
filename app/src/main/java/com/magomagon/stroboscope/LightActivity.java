070e3d9b7790983ff673a834813dcf77b7f31b56
Souhaib Guitouni <ggsouhaib@gmail.com>
2016-05-05 18:32:03 +0200

    [ADD] adding behaviour

--- app/src/main/java/com/magomagon/stroboscope/LightActivity.java
+++ app/src/main/java/com/magomagon/stroboscope/LightActivity.java
@@ -1,16 +1,35 @@
 package com.magomagon.stroboscope;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
 public class LightActivity extends AppCompatActivity {
+
+    private RelativeLayout lightView;
+    private boolean darkState = true;
+    Handler lightHandler = new Handler();
+
+    Runnable periodicChange = new Runnable() {
+        public void run() {
+            if(darkState){
+                lightView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.white));
+            } else {
+                lightView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.black));
+            }
+            darkState = !darkState;
+            lightHandler.postDelayed(periodicChange, 2000);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
 
-
-        //TODO start light here
+        lightView = (RelativeLayout) findViewById(R.id.light_view);
+        lightHandler.post(periodicChange);
     }
 }
